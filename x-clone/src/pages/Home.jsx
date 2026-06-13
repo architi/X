48d8070c4c9d966403497005dad73de1b5a32ca4
architi <aryaamightbeclueless@gmail.com>
2026-06-14 02:30:08 +0530

    home page

---
+++ x-clone/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+function Home() {
+  return (
+    <div
+      style={{
+        color: "#fff",
+        backgroundColor: "#000",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Home feed coming soon</h1>
+    </div>
+  );
+}
+
+export default Home;
