4d7f9085e0e323e69745cb11002c23cc78a6e9ec
architi <aryaamightbeclueless@gmail.com>
2026-06-27 23:41:23 +0530

    fix

--- x-clone/src/pages/Home.jsx
+++ x-clone/src/pages/Home.jsx
@@ -1,18 +1,27 @@
-import Sidebar from ".../components/Sidebar";
+import Sidebar from '../components/Sidebar'
 
-//home page 
 function Home() {
   return (
-    <div className="flex bg-black min-h-screen text-white">
+    <div className='flex bg-black min-h-screen text-white'>
+      
       <Sidebar />
-      <div className="ml-[275px] flex-1 max-w-[600px] border-r border-[#2f3336]">
-        <div className="p-4 border-b border-[#2f3336] text-xl font-bold sticky top-0 bg-black">
-          Home
+
+      <main className='ml-[275px] flex-1 max-w-[600px] border-r border-[#2f3336]'>
+        
+        {/* Header */}
+        <div className='sticky top-0 bg-black border-b border-[#2f3336] px-4 py-3'>
+          <h1 className='text-xl font-bold'>Home</h1>
         </div>
-        <div className="p-4 text-[#71767b]">Feed will go here</div>
-      </div>
+
+        {/* Feed */}
+        <div className='p-4 text-[#71767b]'>
+          Feed will go here
+        </div>
+
+      </main>
+
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
